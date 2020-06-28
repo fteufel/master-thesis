e0184eb3383d001592410d81ae3fcbe3abf7c37a
fteufel <felix.teufel@gmail.com>
2020-06-28 18:00:04 +0200

    fix padding_idx None cuda bug

--- models/awd_lstm.py
+++ models/awd_lstm.py
@@ -336,7 +336,7 @@ class ProteinAWDLSTMModel(ProteinAWDLSTMAbstractModel):
 
         padding_idx = embed.padding_idx
         if padding_idx is None:
-            padding_idx = -1
+            padding_idx = torch.tensor(-1)
 
         X = torch.nn.functional.embedding(words, masked_embed_weight,
             padding_idx, embed.max_norm, embed.norm_type,
