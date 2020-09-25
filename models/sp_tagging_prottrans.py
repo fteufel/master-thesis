5f972cbb54937e4b7dc877cb1e281850b42fd5ff
fteufel <fteufel@student.ethz.ch>
2020-09-25 15:59:09 +0200

    updated bert

--- models/sp_tagging_prottrans.py
+++ models/sp_tagging_prottrans.py
@@ -100,7 +100,7 @@ class XLNetSequenceTaggingCRF(XLNetPreTrainedModel):
             self.outputs_to_emissions = RecurrentOutputsToEmissions(config.hidden_size, config.classifier_hidden_size, config.num_labels, batch_first = True)
         self.crf = CRF(num_tags = config.num_labels, batch_first = True)
 
-        self.global_classifier = nn.Sequential(nn.Linear(config.num_labels, config.num_global_labels), nn.LogSoftmax(dim = -1)) #TODO with my use mode this would be binary crossentropy
+        self.global_classifier = nn.Sequential(nn.Linear(config.num_labels, config.num_global_labels), nn.LogSoftmax(dim = -1))
 
         self.init_weights()
 
@@ -257,43 +257,63 @@ class BertSequenceTaggingCRF(BertPreTrainedModel):
     '''
     def __init__(self, config):
         super().__init__(config)
-        self.use_crf = config.use_crf
-        self.num_global_labels = config.num_global_labels
+        self.use_crf = config.use_crf if hasattr(config, 'use_crf') else True
+        self.num_global_labels = config.num_global_labels if hasattr(config, 'num_global_labels') else config.num_labels
         self.num_labels = config.num_labels
-        self.use_rnn = config.use_rnn
-        self.global_label_loss_multiplier = config.global_label_loss_multiplier
+        self.use_rnn = config.use_rnn if hasattr(config, 'use_rnn') else False
+        self.lm_output_dropout = nn.Dropout(config.lm_output_dropout if hasattr(config, 'lm_output_dropout') else 0) #for backwards compatbility
+        self.lm_output_position_dropout = SequenceDropout(config.lm_output_position_dropout if hasattr(config, 'lm_output_position_dropout') else 0)
+        self.global_label_loss_multiplier = config.global_label_loss_multiplier if hasattr(config, 'global_label_loss_multiplier') else 0
 
+        #more crf states, do not use linear layer to get global probs
+        self.use_large_crf = config.use_large_crf if hasattr(config, 'use_large_crf') else False
         self.transformer = BertModel(config = config)
-        self.outputs_to_emissions = nn.Sequential(nn.Linear(config.hidden_size, config.classifier_hidden_size), 
+        if config.classifier_hidden_size >0:
+            self.outputs_to_emissions = nn.Sequential(nn.Linear(config.hidden_size, config.classifier_hidden_size), 
                                                   nn.ReLU(),
                                                   nn.Linear(config.classifier_hidden_size, config.num_labels),
                                                 )
+        else:
+            self.outputs_to_emissions = nn.Linear(config.hidden_size, config.num_labels) #try no MLP
         if self.use_rnn:
             self.outputs_to_emissions = RecurrentOutputsToEmissions(config.hidden_size, config.classifier_hidden_size, config.num_labels, batch_first = True)
         self.crf = CRF(num_tags = config.num_labels, batch_first = True)
 
-        self.global_classifier = nn.Sequential(nn.Linear(config.num_labels, config.num_global_labels), nn.LogSoftmax(dim = -1)) #TODO with my use mode this would be binary crossentropy
-
+        self.global_classifier = nn.Sequential(nn.Linear(config.num_labels, config.num_global_labels), nn.LogSoftmax(dim = -1))
         self.init_weights()
 
-    def forward(self, input_ids, input_mask=None, targets =None, global_targets = None):
+
+    def forward(self, input_ids = None, input_mask=None, targets =None, global_targets = None, return_both_losses = False, inputs_embeds = None,
+                sample_weights = None, return_logits = False):
         '''Predict sequence features.
         Inputs:  input_ids (batch_size, seq_len)
                  targets (batch_size, seq_len). number of distinct values needs to match config.num_labels
+                 global_targets (batch_size)
+                 input_mask (batch_size, seq_len). binary tensor, 0 at padded positions
+                 return_both_losses: return per_position_loss and global_loss instead of the sum. Use for debugging/separate optimizing
+                 input_embeds: Optional instead of input_ids. Start with embedded sequences instead of token ids.
+                 sample_weights (batch_size) float tensor. weight for each sequence to be used in cross-entropy.
+                 return_logits: bool. Only return the sequence-wise logits (used for SMART regularization)
         Outputs: (loss: torch.tensor)
                  global_probs: global label probs (batch_size, num_labels)
                  probs: model probs (batch_size, seq_len, num_labels)
                  pos_preds: best label sequences (batch_size, seq_len)                 
         '''
-        outputs = self.transformer(input_ids, attention_mask = input_mask) # Returns tuple. pos 0 is sequence output, rest optional.
+        if input_ids is not None and inputs_embeds is not None:
+            raise ValueError("You cannot specify both input_ids and inputs_embeds at the same time")
+        outputs = self.transformer(input_ids, attention_mask = input_mask, inputs_embeds = inputs_embeds) # Returns tuple. pos 0 is sequence output, rest optional.
+        
         sequence_output = outputs[0]
         # trim CLS and SEP token from sequence
         sequence_output = sequence_output[:,1:-1,:]
-
+        if input_mask  is not None:
+            input_mask = input_mask[:,1:-1]
+        #apply dropouts
+        sequence_output = self.lm_output_dropout(sequence_output)
         prediction_logits = self.outputs_to_emissions(sequence_output)
 
         if self.use_crf == True:
-            probs, viterbi_paths = self.crf(prediction_logits) #NOTE do not use loss implemented in this layer, so that I can compare directly to use_crf==False
+            probs, viterbi_paths = self.crf(prediction_logits, mask = input_mask) #NOTE do not use loss implemented in this layer, so that I can compare directly to use_crf==False
             log_probs = torch.log(probs)
             #pad the viterbi paths
             max_pad_len = max([len(x) for x in viterbi_paths])
@@ -305,28 +325,80 @@ class BertSequenceTaggingCRF(BertPreTrainedModel):
             probs =  torch.exp(log_probs)
             pos_preds = torch.argmax(probs, dim =1)
 
-        pooled_outputs = probs.mean(axis =1) #mean over seq_len
-        global_log_probs = self.global_classifier(pooled_outputs)
-        global_probs = torch.exp(global_log_probs)
+        if not self.use_large_crf:
+            pooled_outputs = probs.mean(axis =1) #mean over seq_len
+            global_log_probs = self.global_classifier(pooled_outputs)
+            global_probs = torch.exp(global_log_probs)
+
+        else:
+            global_probs = self.compute_global_labels(probs, input_mask)
+            global_log_probs = torch.log(global_probs) #for compatbility, when providing global labels. Don't need global labels for training large crf.
 
         outputs = (global_probs, probs, pos_preds) #+ outputs
+
+        if return_logits:
+            return log_probs
 
         #get the losses
         losses = 0
         if global_targets is not None: 
-            loss_fct = nn.NLLLoss(ignore_index=-1)
-            loss = loss_fct(
+            loss_fct = nn.NLLLoss(ignore_index=-1, reduction = 'none' if sample_weights is not None else 'mean')
+            global_loss = loss_fct(
                 global_log_probs.view(-1, self.num_global_labels), global_targets.view(-1))
-            losses = losses + loss * self.global_label_loss_multiplier
+
+            if sample_weights is not None:
+                global_loss = global_loss*sample_weights
+                global_loss = global_loss.mean()
+            losses = losses + global_loss * self.global_label_loss_multiplier
 
         if targets is not None:
-            loss_fct = nn.NLLLoss(ignore_index=-1)
+            loss_fct = nn.NLLLoss(ignore_index=-1, reduction = 'none' if sample_weights is not None else 'mean')
             loss = loss_fct(
                 log_probs.view(-1, self.config.num_labels), targets.view(-1))
+
+            if sample_weights is not None:
+                #make sample weights to size batch_size x seq_len
+                sample_weights_tiled = sample_weights.unsqueeze(-1).expand_as(targets)
+                loss = loss * sample_weights_tiled.reshape(-1) #expand_as not contiguous in memory.
+                loss = loss.mean()
             losses = losses + loss
         
         if targets is not None or global_targets is not None:
-            outputs = (losses,) + outputs
+            if return_both_losses:
+                outputs = (loss, global_loss,) + outputs
+            else:
+                outputs = (losses,) + outputs
         
         #loss, global_probs, pos_probs, pos_preds
         return outputs
+
+    def compute_global_labels(self, probs, mask):
+        '''Compute the global labels as sum over marginal probabilities, normalizing by seuqence length.
+        For agrregation, the EXTENDED_VOCAB indices from signalp_dataset.py are hardcoded here.
+        If num_global_labels is 2, assume we deal with the sp-no sp case.
+        '''
+        #probs = b_size x seq_len x n_states tensor 
+        #Yes, each SP type will now have 4 labels in the CRF. This means that now you only optimize the CRF loss, nothing else. 
+        # To get the SP type prediction you have two alternatives. One is to use the Viterbi decoding, 
+        # if the last position is predicted as SPI-extracellular, then you know it is SPI protein. 
+        # The other option is what you mention, sum the marginal probabilities, divide by the sequence length and then sum 
+        # the probability of the labels belonging to each SP type, which will leave you with 4 probabilities.
+        if mask is None:
+            mask = torch.ones(probs.shape[0], probs.shape[1], device = probs.device)
+
+        #TODO check unsqueeze ops for division/multiplication broadcasting
+        summed_probs = (probs *mask.unsqueeze(-1)).sum(dim =1) #sum probs for each label over axis
+        sequence_lengths = mask.sum(dim =1)
+        global_probs = summed_probs/sequence_lengths.unsqueeze(-1)
+
+        #aggregate
+        no_sp = global_probs[:,0:3].sum(dim=1) 
+        spi = global_probs[:,3:7].sum(dim =1)
+
+        if self.num_global_labels >2:
+            spii =global_probs[:, 7:11].sum(dim =1)
+            tat = global_probs[:, 11:].sum(dim =1)
+            return torch.stack([no_sp, spi, spii, tat], dim =-1)
+        
+        else:
+            return torch.stack([no_sp, spi], dim =-1)
