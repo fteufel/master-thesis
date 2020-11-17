- fixed metrics_utils bug - crf constraints added to training script - alternative mode of region length calculation (currently disabled) - refactored regioncrfdataset

train_scripts/downstream_tasks/train_multistate_crf_model.py
@@ -399,7 +399,12 @@ def main_training_loop(args: argparse.ArgumentParser):
 
 
     # Set seed
-    seed = torch.seed()
+    if args.random_seed is not None:
+        torch.manual_seed(args.random_seed)
+        seed = args.random_seed
+    else:
+        seed = torch.seed()
+
     logger.info(f'torch seed: {seed}')
     wandb.config.update({'seed': seed})
 
@@ -433,6 +438,43 @@ def main_training_loop(args: argparse.ArgumentParser):
     if args.kingdom_embed_size > 0:
         setattr(config, 'use_kingdom_id', True)
         setattr(config, 'kingdom_embed_size', args.kingdom_embed_size)
+
+
+    #hardcoded for full model, 5 classes, 37 tags
+    if args.constrain_crf:
+        allowed_transitions = [
+            
+            #NO_SP
+            (0,0), (0,1), (1,1), (1,2), (1,0), (2,1), (2,2), # I-I, I-M, M-M, M-O, M-I, O-M, O-O
+            #SPI
+            #3 N, 4 H, 5 C, 6 I, 7M, 8 O
+            (3,3), (3,4), (4,4), (4,5), (5,5), (5,8), (8,8), (8,7), (7,7), (7,6), (6,6), (6,7), (7,8), 
+            
+            #SPII
+            #9 N, 10 H, 11 CS, 12 C1, 13 I, 14 M, 15 O
+            (9,9), (9,10), (10,10), (10,11), (11,11), (11,12), (12,15), (15,15), (15,14), (14,14), (14,13), (13,13), (13,14), (14,15),
+            
+            #TAT
+            #16 N, 17 RR, 18 H, 19 C, 20 I, 21 M, 22 O
+            (16,16), (16,17), (17,17), (17,16), (16,18), (18,18), (18,19), (19,19), (19,22), (22,22), (22,21), (21,21),(21,20), (20,20), (20,21), (21,22),
+            
+            #TATLIPO
+            #23 N, 24 RR, 25 H, 26 CS, 27 C1, 28 I, 29 M, 30 O
+            (23,23), (23,24), (24,24), (24,23), (23,25), (25,25), (25,26), (26,26), (26,27), (27,30), (30,30), (30,29), (29,29), (29,28), (28,28), (28,29),(29,30),
+            
+            #PILIN
+            #31 P, 32 CS, 33 H, 34 I, 35 M, 36 O
+            #TODO check transition from 33: to M or to O. Need to fix when making real h-region labels (so far ignoring TM info, just 10 pos h)
+            (31,31), (31,32), (32,32), (32,33), (33,33), (33,36), (36,36), (36,35), (35,35), (35,34), (34,34), (34,35), (35,36)
+            
+        ]
+            
+        allowed_starts = [0, 1, 3, 9, 16, 23, 31]
+        allowed_ends = [0,1,2, 13,14,15, 20,21,22, 28,29,30, 34,35,36]
+
+        setattr(config, 'allowed_crf_transitions', allowed_transitions)
+        setattr(config, 'allowed_crf_starts', allowed_starts)
+        setattr(config, 'allowed_crf_ends', allowed_ends)
 
 
     #setattr(config, 'gradient_checkpointing', True) #hardcoded when working with 256aa data
@@ -686,6 +728,7 @@ if __name__ == '__main__':
                         help='upsample all kingdoms to equal probabilities')
     parser.add_argument('--use_global_targets', action='store_true',
                         help='Compute and add loss of global label classification')
+    parser.add_argument('--random_seed', type=int, default=None, help='random seed for torch.')
 
     #args for model architecture
     parser.add_argument('--model_architecture', type=str, default = 'bert',
@@ -701,6 +744,7 @@ if __name__ == '__main__':
     parser.add_argument('--use_cs_tag', action='store_true', help='Replace last token of SP with C for cleavage site')
     parser.add_argument('--kingdom_as_token', action='store_true', help='Kingdom ID is first token in the sequence')
     parser.add_argument('--sp_region_labels', action='store_true', help='Use labels for n,h,c regions of SPs.')
+    parser.add_argument('--constrain_crf', action='store_true', help='Constrain the transitions of the region-tagging CRF.')
 
 
     args = parser.parse_args()

train_scripts/utils/region_similarity.py
@@ -188,6 +188,33 @@ NO_H_CLASSES = [0,5]
 NO_C_CLASSES = [0,2,4,5]
 
 
+def get_region_start_end(tag_sequence, token_ids:List[int]):
+    '''end_idx defined as last idx that has this value.
+    Add +1 when [start, end] indexing.'''
+
+    if type(tag_sequence) == np.ndarray:
+        tag_sequence = list(tag_sequence)
+
+    #TODO catch ValueError when id not in tag_sequence
+    start_idx = 1000
+    end_idx = -1
+    for token_id in token_ids:
+        try:
+            start= tag_sequence.index(token_id)
+            end= len(tag_sequence) -1 - tag_sequence[::-1].index(token_id)
+
+            start_idx = min(start_idx,start)
+            end_idx = max(end_idx, end)
+        #.index() throws error when no match is found.
+        except ValueError:
+            pass
+    
+    if start_idx == 1000 or end_idx ==-1:
+        return 0,0
+    else:
+        return start_idx, end_idx
+
+
 def get_region_lengths(tag_sequences: Union[List[np.ndarray], List[List[int]], np.ndarray], 
                         class_labels: np.ndarray,
                         sp_lengths: np.ndarray = None,
@@ -200,7 +227,7 @@ def get_region_lengths(tag_sequences: Union[List[np.ndarray], List[List[int]], n
                         agg_fn = 'mean',
                         ):
     '''Calculate the mean length of each region.
-    Only counts tags in a sequence, does not check whether the tags are contiguous
+    Only counts tags in a sequence, does not check whether the tags are contiguous TODO this causes problems.
     Inputs:
         tag_sequences: viterbi path of each sequence
         class_labels: global label of each sequence
@@ -233,6 +260,23 @@ def get_region_lengths(tag_sequences: Union[List[np.ndarray], List[List[int]], n
         h_tags = np.array([np.isin(np.array(x), tag_sequences).sum() for x in tag_sequences])
         c_tags = np.array([np.isin(np.array(x), tag_sequences).sum() for x in tag_sequences])
 
+
+    computation_mode = 'x'
+    if computation_mode == 'region_borders':
+        n_tags = []
+        h_tags = []
+        c_tags = []
+        for seq in tag_sequences:
+            start,end = get_region_start_end(seq, n_region_tags)
+            n_tags.append(end+1-start)
+            start,end = get_region_start_end(seq, h_region_tags)
+            h_tags.append(end+1-start)
+            start,end = get_region_start_end(seq, c_region_tags)
+            c_tags.append(end+1-start)
+
+        n_tags = np.array(n_tags)
+        h_tags = np.array(h_tags)
+        c_tags = np.array(c_tags)
 
     if sp_lengths is not None:
         assert len(sp_lengths) == len(tag_sequences), "lengths of input arrays don't match"

train_scripts/utils/signalp_dataset.py
@@ -402,7 +402,7 @@ class LargeCRFPartitionDataset(PartitionThreeLineFastaDataset):
 
 
 #clean implementation without inheritance from base classes. This will hopefully be the definitive one.
-class RegionCRFDataset(PartitionThreeLineFastaDataset):
+class RegionCRFDatasetOld(PartitionThreeLineFastaDataset):
     '''Converts label sequences to array for multi-state crf'''
     def __init__(self,
             data_path: Union[str, Path],
@@ -424,6 +424,191 @@ class RegionCRFDataset(PartitionThreeLineFastaDataset):
         self.label_vocab = label_vocab #None is fine, process_SP will use default
         self.add_global_label = add_global_label
         self.global_label_dict = global_label_dict if global_label_dict is not None else SIGNALP6_GLOBAL_LABEL_DICT
+
+    def __getitem__(self, index):
+        item = self.sequences[index]
+        labels = self.labels[index]
+        global_label = self.global_labels[index]
+        weight = self.sample_weights[index] if hasattr(self, 'sample_weights') else None
+        kingdom_id =  SIGNALP_KINGDOM_DICT[self.kingdom_ids[index]] if hasattr(self, 'kingdom_ids') else None
+
+        
+
+        if self.add_special_tokens == True:
+            token_ids = self.tokenizer.encode(item, kingdom_id = self.kingdom_ids[index], 
+                                              label_id = global_label if self.add_global_label else None)
+
+        else: 
+            token_ids = self.tokenizer.tokenize(item)# + [self.tokenizer.stop_token]
+            token_ids = self.tokenizer.convert_tokens_to_ids(token_ids)
+
+
+
+        label_matrix = process_SP(labels, item, sp_type=global_label, vocab=self.label_vocab)
+        global_label_id = self.global_label_dict[global_label]
+
+        input_mask = np.ones_like(token_ids)
+
+        #also need to return original tags or cs
+        if global_label == 'NO_SP':
+            cs = -1
+        elif global_label == 'SP':
+            cs = labels.rfind('S') +1 #+1 for compatibility. CS reporting uses 1-based instead of 0-based indexing
+        elif global_label == 'LIPO':
+            cs = labels.rfind('L') +1
+        elif global_label == 'TAT':
+            cs = labels.rfind('T') +1
+        elif global_label == 'TATLIPO':
+            cs = labels.rfind('T') +1
+        elif global_label == 'PILIN':
+            cs = labels.rfind('P') +1
+        else:
+            raise NotImplementedError(f'Unknown CS defintion for {global_label}')
+
+        return_tuple = (np.array(token_ids), label_matrix, np.array(input_mask), global_label_id, cs)
+
+        if weight is not None:
+            return_tuple = return_tuple + (weight,)
+        if kingdom_id is not None:
+            return_tuple = return_tuple + (kingdom_id,)
+
+
+        return return_tuple
+
+    #needs new collate_fn, targets need to be padded and stacked.
+    def collate_fn(self, batch: List[Any]) -> Dict[str, torch.Tensor]:
+        #unpack the list of tuples
+        if  hasattr(self, 'sample_weights') and hasattr(self, 'kingdom_ids'):
+            input_ids, label_ids,mask, global_label_ids, cleavage_sites, sample_weights, kingdom_ids = tuple(zip(*batch))
+        elif hasattr(self, 'sample_weights'):
+            input_ids, label_ids,mask, global_label_ids, sample_weights, cs = tuple(zip(*batch))
+        elif hasattr(self, 'kingdom_ids'):
+            input_ids, label_ids,mask, global_label_ids, kingdom_ids, cs = tuple(zip(*batch))
+        else:
+            input_ids, label_ids,mask, global_label_ids = tuple(zip(*batch))
+
+        data = torch.from_numpy(pad_sequences(input_ids, 0))
+        
+        # ignore_index is -1
+        targets = pad_sequences(label_ids, -1)
+        targets = np.stack(targets)
+        targets = torch.from_numpy(targets) 
+        mask = torch.from_numpy(pad_sequences(mask, 0))
+        global_targets = torch.tensor(global_label_ids)
+        cleavage_sites = torch.tensor(cleavage_sites)
+
+        return_tuple = (data, targets, mask, global_targets, cleavage_sites)
+        if  hasattr(self, 'sample_weights'):
+            sample_weights = torch.tensor(sample_weights)
+            return_tuple = return_tuple + (sample_weights,)
+        if hasattr(self, 'kingdom_ids'):
+            kingdom_ids = torch.tensor(kingdom_ids)
+            return_tuple = return_tuple + (kingdom_ids, )
+
+        return return_tuple
+
+
+
+class RegionCRFDataset(Dataset):
+    '''Converts label sequences to array for multi-state crf.
+    data_path:              training set 3-line fasta
+    sample_weights_path:    optional df with a weight for each Entry in data_path
+    tokenizer:              tokenizer to use for conversion of sequences
+    partition_id :          list of partition ids to use
+    kingdom_id:             list of kingdom ids to use
+    type_id :               list of type ids to use
+    add_special_tokens:     add cls, sep tokens to sequence
+    label_vocab:            str-int mapping for label sequences
+    global_label_dict:      str-int mapping for global labels
+    positive_samples_weight: optional weight that is returned for positive samples 
+                             (use for loss scaling)
+    return_kingdom_ids:     deprecated, always returning kingdom id now.
+    make_cs_state:          deprecated, no cs state implemented for multi-tag
+    add_global_label:       add the global label as a token to the start of a sequence
+    augment_data_paths:     paths to additional 3-line fasta files with augmented samples.
+                            are added to the real data.
+    '''
+    def __init__(self,
+            data_path: Union[str, Path],
+            sample_weights_path = None,
+            tokenizer: Union[str, PreTrainedTokenizer] = 'iupac',
+            partition_id: List[str] = [0,1,2,3,4],
+            kingdom_id: List[str] = ['EUKARYA', 'ARCHAEA', 'NEGATIVE', 'POSITIVE'],
+            type_id: List[str] = ['LIPO', 'NO_SP', 'SP', 'TAT', 'TATLIPO', 'PILIN'],
+            add_special_tokens = False,
+            label_vocab = None,
+            global_label_dict = None,
+            positive_samples_weight = None, 
+            return_kingdom_ids = False, # legacy
+            make_cs_state = False, #legacy to not break code when just plugging in this dataset
+            add_global_label = False,
+            augment_data_paths: List[Union[str, Path]] = None,
+            ):
+
+
+        super().__init__()
+
+        # set up parameters
+
+        self.data_file = Path(data_path)
+        if not self.data_file.exists():
+            raise FileNotFoundError(self.data_file)
+
+        self.add_special_tokens = add_special_tokens
+
+        self.label_tokenizer = SP_label_tokenizer()
+        
+        if isinstance(tokenizer, str):
+            from tape import TAPETokenizer
+            tokenizer = TAPETokenizer(vocab=tokenizer)
+        self.tokenizer = tokenizer
+
+        self.label_vocab = label_vocab #None is fine, process_SP will use default
+        self.add_global_label = add_global_label
+        self.global_label_dict = global_label_dict if global_label_dict is not None else SIGNALP6_GLOBAL_LABEL_DICT
+
+        self.type_id = type_id
+        self.partition_id = partition_id
+        self.kingdom_id = kingdom_id
+        
+        # Load and filter the data
+        
+        self.identifiers, self.sequences, self.labels = parse_threeline_fasta(self.data_file)
+
+        if augment_data_paths is not None:
+            for path in augment_data_paths:
+                ids, seqs, labs =  parse_threeline_fasta(path)
+                self.identifiers = self.identifiers + ids
+                self.sequences = self.sequences + seqs
+                self.labels =  self.labels + labs
+
+        self.identifiers, self.sequences, self.labels = subset_dataset(self.identifiers, self.sequences, self.labels, partition_id, kingdom_id, type_id)
+        self.global_labels = [x.split('|')[2] for x in self.identifiers]
+        self.kingdom_ids = [x.split('|')[1] for x in self.identifiers]
+
+        #make kingdom-balanced sampling weights to use if needed
+        count_dict = defaultdict(lambda: 0)
+        for x in self.kingdom_ids:
+            count_dict[x] +=1
+        self.balanced_sampling_weights = [1.0/count_dict[i] for i in self.kingdom_ids]
+
+
+        # make sample weights for either loss scaling or balanced sampling, if none defined weight=1 for each item
+        if sample_weights_path is not None:
+            sample_weights_df = pd.read_csv(sample_weights_path, index_col = 0)
+            subset_ids = [x.split('|')[0].lstrip('>') for x in self.identifiers]
+            df_subset = sample_weights_df.loc[subset_ids]
+            self.sample_weights = list(df_subset['0'])
+        elif positive_samples_weight is not None:
+            #make weights from global labels
+            self.sample_weights = [positive_samples_weight if label in ['SP', 'LIPO', 'TAT'] else 1 for label in self.global_labels]
+        #NOTE this is just to make the training script more adaptable without having to change batch handling everytime. Always make weights, 
+        # decide in training script whether or not to use
+        else:
+            self.sample_weights = [1 for label in self.global_labels]
+
+    def __len__(self) -> int:
+        return len(self.sequences)
 
     def __getitem__(self, index):
         item = self.sequences[index]

train_scripts/utils/signalp_label_processing.py
@@ -214,12 +214,19 @@ def process_SP(label_sequence: str, aa_sequence: str, sp_type=str, vocab: Dict[s
 
         n_end = hydrophobic_pos
         h_start = 2
-        h_end = last_idx-1
+        h_end = last_idx-2 #last 3 residues of SP are always C
+
+        #NOTE when last_idx-2 == hydrophobic_pos, h_end does not work. 
+        # Very short seqs can have this problem. So far only happened once
+        # This breaks the last 3=c rule, but otherwise hydrophobic_pos would be c.
+        if h_end == hydrophobic_pos:
+            h_end = hydrophobic_pos+1
         c_start = hydrophobic_pos+1
 
         tag_matrix[:n_end, vocab['SP_N']] =1
         tag_matrix[h_start:h_end, vocab['SP_H']] =1
         tag_matrix[c_start:last_idx+1, vocab['SP_C']] =1
+
 
     elif sp_type == 'LIPO':
         tag_matrix[intracellular_idx, vocab['LIPO_I']] = 1
@@ -316,5 +323,5 @@ def process_SP(label_sequence: str, aa_sequence: str, sp_type=str, vocab: Dict[s
 
 
     #quality control: at least 1 state active at each position
-    assert not any(tag_matrix.sum(axis=1) ==0), 'Processing failed. There are positions where no state was set active.'
+    assert not any(tag_matrix.sum(axis=1) ==0), f'Processing {sp_type} failed. There are positions where no state was set active.'
     return tag_matrix